25501a7adc43c4923017e260cde088c926ccc4d7
metelaifne <metelaifne@gmail.com>
2018-08-29 16:37:11 +0800

    update

--- wp/xmlrpc0db0.php
+++ wp/xmlrpc0db0.php
@@ -2,13 +2,13 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>http://musicisgood.org</homePageLink>
+    <homePageLink>https://www.metelaifne.ga/</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="http://musicisgood.org/wp/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://musicisgood.org/wp/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://musicisgood.org/wp/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="http://musicisgood.org/wp/xmlrpc.php" />
-      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://musicisgood.org/wp-json/" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://www.metelaifne.ga//wp/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.metelaifne.ga//wp/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.metelaifne.ga//wp/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://www.metelaifne.ga//wp/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.metelaifne.ga//wp-json/" />
 	    </apis>
   </service>
 </rsd>
